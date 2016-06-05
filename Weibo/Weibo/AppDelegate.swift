c6535cb0cce64fc8fab01db694a2544640d92db7
sw <943886881@qq.com>
2016-06-06 01:32:12 +0800

    自定义LOG

--- Weibo/Weibo/AppDelegate.swift
+++ Weibo/Weibo/AppDelegate.swift
@@ -12,7 +12,7 @@ import UIKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
-
+    
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
@@ -20,8 +20,20 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         window?.rootViewController = UIViewController()
         window?.backgroundColor = UIColor.whiteColor()
         window?.makeKeyAndVisible()
+        WSLog(10086)
+
         return true
     }
-
+}
+/*
+// 以下的写法在Swift3.0中将被废弃，改为#line这种写法
+print(__LINE__) // 打印行号
+print(__FUNCTION__) // 打印函数名
+print(__FILE__) // 打印文件路径
+*/
+func WSLog<T>(message: T, file:String = #file, method:String = #function, line: Int = #line) {
+    #if DEBUG
+    print("\(method)[\(line)]: \(message)")
+    #endif
 }
 
