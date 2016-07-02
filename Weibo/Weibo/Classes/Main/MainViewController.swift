fb0c6424ed02636b166d16b1d4e94e8a0d53b5cc
sw <943886881@qq.com>
2016-07-02 16:35:16 +0800

    as! 改为as?

--- Weibo/Weibo/Classes/Main/MainViewController.swift
+++ Weibo/Weibo/Classes/Main/MainViewController.swift
@@ -26,7 +26,7 @@ class MainViewController: UITabBarController {
             let dicArr = try NSJSONSerialization.JSONObjectWithData(data, options: NSJSONReadingOptions.MutableContainers)
             for dic in dicArr as! [[String:AnyObject]]
             {
-                addChildViewController(dic["vcName"] as! String, image: dic["imageName"] as! String, highLightImage: dic["imageName"] as! String + "_highlighted", title: dic["title"] as! String)
+                addChildViewController(dic["vcName"] as? String, image: dic["imageName"] as? String, highLightImage: (dic["imageName"] as? String)! + "_highlighted", title: dic["title"] as? String)
             }
         } catch{
             addChildViewController("HomeTableViewController", image: "tabbar_home", highLightImage: "tabbar_home_highlighted", title: "首页")
@@ -40,7 +40,7 @@ class MainViewController: UITabBarController {
 
 
     // MARK: 内部控制方法
-    func addChildViewController(childControllerName: String, image: String, highLightImage: String, title:String) {
+    func addChildViewController(childControllerName: String?, image: String?, highLightImage: String?, title:String?) {
         
         let nameSpace = NSBundle.mainBundle().infoDictionary!["CFBundleExecutable"]
         // 可选绑定
@@ -49,7 +49,11 @@ class MainViewController: UITabBarController {
             return
         }
         //if let ns = nameSpace as? String {
-        let cls : AnyClass? = NSClassFromString(ns + "." + childControllerName)
+        guard let vcName = childControllerName else {
+            WSLog("控制器名称不能为nil")
+            return
+        }
+        let cls : AnyClass? = NSClassFromString(ns + "." + vcName)
         // AnyClass本质是AnyObject.Type类型
         // UIViewController本质是UIViewController.Type类型
         // 将AnyClass转为UIViewController类型 
@@ -66,9 +70,18 @@ class MainViewController: UITabBarController {
             // 如果是在iOS8以前，只有文字有效果，而图片没效果
             tabBar.tintColor = UIColor.orangeColor()
             // 控制器选项卡属性
-            childController.tabBarItem.image = UIImage(named: image)
-            childController.tabBarItem.selectedImage = UIImage(named: highLightImage)
-            childController.title = title
+        guard let iName = image else {
+            return
+        }
+            childController.tabBarItem.image = UIImage(named: iName)
+        guard let highIName = highLightImage else {
+            return
+        }
+            childController.tabBarItem.selectedImage = UIImage(named: highIName)
+        guard let titleName = title else {
+            return
+        }
+            childController.title = titleName
             // 包装导航控制器
             let  nav = UINavigationController(rootViewController: childController)
             // 添加子控制器
